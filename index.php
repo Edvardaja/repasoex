5e77441dbc254d150253035ddf425e82d2e2e1ab
Edvardaja <edjaxx@alumnatflorida.es>
2025-11-18 19:08:28 +0100

    Anadido referencias estillos.css en php

--- index.php
+++ index.php
@@ -49,16 +49,19 @@ DAW | Despliegue de Aplicaciones Web
 <head>
  <meta charset="UTF-8">
  <title>Ahorcado</title>
+ <link> rel="stylesheet" href="estilos.css"</link>
 </head>
 <body>
+    <div class="container">
  <h1>Juego del Ahorcado</h1>
  <p>Palabra secreta: <?php echo $_SESSION['letras_acertadas']; ?></p>
  <p>Vidas restantes: <?php echo $_SESSION['vidas']; ?></p>
- <form method="post">
+ <form class="formulario" method="post">
  <label for="letra">Introduce una letra:</label>
  <input type="text" name="letra" id="letra" maxlength="1" required>
  <button type="submit">Adivinar</button>
  </form>
  <p>Letras usadas: <?php echo implode(', ', $_SESSION['letras_usadas']); ?></p>
+ </div>
 </body>
 </html>
